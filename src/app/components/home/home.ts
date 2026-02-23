f5876979d2c79a5e571863501ecb2956f088194a
dianav <vararean.diana@gmail.com>
2026-02-23 13:45:28 +0200

    feat: implement home page hero section

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [ FormsModule],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
+  searchQuery: string = '';
 
+  onSearch(){
+    // TODO: Implement search functionality
+  }
 }
