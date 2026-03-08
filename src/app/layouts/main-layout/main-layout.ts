4e383e6619776a993a980201a04516ae4270de91
dianav <vararean.diana@gmail.com>
2026-03-08 16:17:02 +0200

    feat: auto-close sidebar on scroll

--- src/app/layouts/main-layout/main-layout.ts
+++ src/app/layouts/main-layout/main-layout.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Sidebar } from '../../components/sidebar/sidebar';
 import { Navbar } from '../../components/navbar/navbar';
+import { SidebarService } from '../../services/sidebar.service';
 
 @Component({
   selector: 'app-main-layout',
@@ -11,5 +12,11 @@ import { Navbar } from '../../components/navbar/navbar';
   styleUrl: './main-layout.scss',
 })
 export class MainLayout {
+  private sidebarService = inject(SidebarService);
 
+  onContentScroll() {
+    if (this.sidebarService.isExpanded()) {
+      this.sidebarService.close();
+    }
+  }
 }
